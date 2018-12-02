5269035dbb97fd98f180223b8b67911ae8f437a4
jeffreyhanson <jeffrey.hanson@uqconnect.edu.au>
2018-12-02 21:36:25 +1000

    update docs for sim data

--- R/data.R
+++ R/data.R
@@ -18,17 +18,6 @@ NULL
 #'
 #'       \item{\code{"name"}}{\code{character} name for each project.}
 #'
-#'       \item{\code{"cost"}}{\code{numeric} cost for each project.}
-#'
-#'       \item{\code{"success"}}{\code{numeric} probability of each project
-#'         succeeding if it is funded.}
-#'
-#'       \item{\code{"locked_in"}}{\code{logical} indicating if certain
-#'         projects should be locked into the solution.}
-#'
-#'       \item{\code{"locked_out"}}{\code{logical} indicating if certain
-#'         projects should be locked out of the solution.}
-#'
 #'       \item{\code{"S1"} ... \code{"S5"}}{\code{numeric} columns for each
 #'       species (i.e. \code{"S1"}, \code{"S2"}, \code{"S3"}, \code{"S4"},
 #'       \code{"S5"}, indicating the enhanced probability that each
@@ -36,21 +25,54 @@ NULL
 #'
 #'     }}
 #'
-#'   \item{\code{sim_tree}}{A \code{\link[ape]{phylo}} phylogenetic tree
-#'     that describes the evolutionary relationships between five simulated
-#'     species. These species are named: \code{"S1"}, \code{"S2"}, \code{"S3"},
-#'     \code{"S4"}, and \code{"S5"}.}
+#'   \item{\code{sim_action_data}}{A \code{\link[base]{data.frame}} containing
+#'     data for six simulated actions. Each row corresponds to a
+#'     different action and each column contains information about the
+#'     actions. This table contains the following columns:
+#'
+#'     \describe{
+#'
+#'       \item{\code{"name"}}{\code{character} name for each action.}
+#'
+#'       \item{\code{"cost"}}{\code{numeric} cost for each action.}
+#'
+#'       \item{\code{"success"}}{\code{numeric} probability of each action
+#'         succeeding if it is funded.}
+#'
+#'       \item{\code{"locked_in"}}{\code{logical} indicating if certain
+#'         actions should be locked into the solution.}
+#'
+#'       \item{\code{"locked_out"}}{\code{logical} indicating if certain
+#'         actions should be locked out of the solution.}
+#'
+#'     }}
+#'
+#'     \item{\code{sim_organization_data}}{A \code{\link[tibble]{tibble}}
+#'       indicating which conservation actions fall under which conservation
+#'       projects. It contains the following columns:
+#'
+#'       \describe{
+#'
+#'         \item{\code{"name"}}{\code{character} name for each project.}
+#'
+#'         \item{\code{"S1_action"} ... \code{"SN_action"}}{\code{logical}
+#'           columns for each action, ranging from \code{"S1_action"} to
+#'           \code{"SN_action"} where \code{N} is
+#'           the number of actions (equal to the number of species in this
+#'           simulated data), indicating if an action is associated with a
+#'           project (\code{TRUE}) or not (\code{FALSE}).}
+#'
+#'     }}
+#'
+#'     \item{tree}{\code{\link[ape]{phylo}} phylogenetic tree for the species.}
 #'
 #' }
-#'
-#' @aliases sim_tree
-#'
-#' @usage data(sim_project_data)
-#'
-#' @usage data(sim_tree)
+#' @aliases sim_project_data sim_action_data sim_organization_data sim_tree
 #'
 #' @format \describe{
-#'   \item{sim_project_data}{\code{\link[base]{data.frame}} object}
+#'   \item{sim_project_data}{\code{\link[tibble]{tibble}} object.}
+#'   \item{sim_action_data}{\code{\link[tibble]{tibble}} object.}
+#'   \item{sim_organization_data}{\code{\link[tibble]{tibble}} object.}
 #'   \item{sim_tree}{\code{\link[ape]{phylo}} object.}
 #' }
 #'
@@ -58,10 +80,16 @@ NULL
 #'
 #' @examples
 #' # load data
-#' data(sim_project_data, sim_tree)
+#' data(sim_project_data, sim_action_data, sim_organization_data, sim_tree)
 #'
 #' # print project data
 #' print(sim_project_data)
+#
+#' # print action data
+#' print(sim_action_data)
+#
+#' # print organization data
+#' print(sim_organization_data)
 #
 #' # print phylogenetic tree
 #' print(sim_tree)
@@ -73,7 +101,17 @@ NULL
 NULL
 
 #' @rdname sim_data
+#' @usage data(sim_project_data)
 "sim_project_data"
 
 #' @rdname sim_data
+#' @usage data(sim_action_data)
+"sim_action_data"
+
+#' @rdname sim_data
+#' @usage data(sim_organization_data)
+"sim_organization_data"
+
+#' @rdname sim_data
+#' @usage data(sim_tree)
 "sim_tree"
