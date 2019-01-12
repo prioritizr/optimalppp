a5591b564e93c41d35a834e232849252ba099324
jeffreyhanson <jeffrey.hanson@uqconnect.edu.au>
2019-01-12 17:48:06 +1000

    add notice

--- src/functions.h
+++
@@ -1,9 +0,0 @@
-#ifndef FUNCTIONS_H
-#define FUNCTIONS_H
-
-#include "package.h"
-
-Rcpp::NumericVector ppp_epd(arma::sp_mat, arma::sp_mat, arma::sp_mat,
-                            Rcpp::NumericVector, arma::sp_mat);
-
-#endif
--- tests/testthat.R
+++
@@ -1,6 +0,0 @@
-# load packages
-library(testthat)
-library(optimalppp)
-
-# run tests
-test_check("optimalppp")
